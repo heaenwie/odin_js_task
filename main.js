ee4e32fc65c346f643bf60dff996557580bc0262
heaenwie <marta.eleonora.ciesielska@gmail.com>
2017-04-01 23:15:08 +0200

    Add some styling!

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function printDivs(canvasSize) {
   $("div.canvas-div").remove();
   for(i = 0; i < canvasSize; i++) {
     for(j = 0; j < canvasSize; j++) {
-        document.getElementById('canvas').innerHTML += '<div class="canvas-div">'+j+'</div>';
+        document.getElementById('canvas').innerHTML += '<div class="canvas-div"></div>';
     }
   }
   $( "div.canvas-div" ).css( "width", function( index ) {
@@ -14,7 +14,20 @@ function printDivs(canvasSize) {
   });
 }
 function togglOnHover() {
-  $("div.canvas-div").hover(function() {
+  $("div.canvas-div").mouseover(function() {
     $(this).addClass("hover");
+    $(this).css( "background", function( index ) {
+      return randomColour();
+    });
   })
+}
+
+function randomColour () {
+    // Set variables to number between 0 and 255
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    // Create a string matching rbg(000,000,000) for use in CSS
+    rgb = "rgb(" + r + "," + g +"," + b + ")"
+    return rgb;
 }
